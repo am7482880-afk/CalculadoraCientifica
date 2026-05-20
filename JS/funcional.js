ddc61b3f6586d511e3cbab970b8271e2b5f03476
Nana <am7482880@gmail.com>
2026-05-20 10:38:08 -0500

    commit 3-4

--- JS/funcional.js
+++ JS/funcional.js
@@ -1,49 +1,86 @@
-let  p = {
+//crear las propiedades del objeto
+
+let p = {
     teclas: document.querySelectorAll("#calculadora ul li"),
     accion: null,
     digito: null,
-    operaciones: document.querySelectorAll("#operaciones"),
-    cantidadSignos: 0,
-    cantidadDecimal: false,
+    operacion: document.querySelector("#operaciones"),
+    cantisimbolo: 0,
+    cantdecimal: false,
     resultado: false
 }
 
+//creas los metodos
+
 let m = {
-    inicio: function() 
-    {
-        for(let i = 0; i < p.teclas.length; i++)
-        {
-            p.teclas[i].addEventListener("click", m.oprimirTecla);
-        }
+    inicio: function () {
+               for (let i = 0; i < p.teclas.length; i++) {
+            p.teclas[i].addEventListener("click", m.oprimirtecla);
+        }         
+    
+    }, oprimirtecla: function (tecla) {
+        p.accion = tecla.currentTarget.getAttribute("class");
+         p.digito = tecla.currentTarget.innerHTML; 
+        m.calcular(p.accion, p.digito);
+
+
+
+    }, calcular: function(accion, digito) {
+        switch (accion) {
+        case "numero":
+            p.cantisimbolo = 0;
+            //console.log(" numero");
+            if (p.operacion.innerHTML == "0") {
+                    p.operacion.innerHTML = digito;
+                } else {
+                   // p.operacion.innerHTML += digito;
+                   if(p.resultado){
+                    p.resultado = false;
+                    p.operacion.innerHTML = digito;
+                   }else{
+                    p.operacion.innerHTML += digito;
+                   }
+                }         
+        break;
+
+
+        case "simbolo":
+            p.cantisimbolo++;
+            if (p.cantisimbolo == 1) {
+                if (p.operacion.innerHTML == "0") {
+                    p.operacion.innerHTML = "0" + digito;
+                } else {
+                    p.operacion.innerHTML += digito;
+                    p.cantdecimal = false;
+                }}
+            //console.log("simbolo");
+            break;
+
+
+        case "decimal":
+            if(!p.operacion){
+            //console.log("decimal");
+             p.operacion.innerHTML += digito;
+             p.cantdecimal = true
+            }
+            break;
+
+
+        case "igual":
+            console.log("igual");
+                p.operacion.innerHTML = eval(p.operacion.innerHTML);
+                p.resultado = true;
+            break;
+          }
+     },
 
     
-    },
-    oprimirTecla: function()
-    {
-        p.accion = tecla.target.getAttribute("class");
-        m.calculadora(p.accion);
-
-    },
-    calculadora: function(accion)
-    {
-        switch(accion)
-        {
-            case "numero":
-                console.log("numero");
-            break;
-
-            case "signo":
-                console.log("simbolo");
-            break;
-
-            case "decimal":
-                console.log("decimal");
-            break;
-
-            case "igual":
-                console.log("igual");
-            break;
-        }
-
+    borrarCalculadora: function() {
+        p.operacion.innerHTML = "0";
     }
 }
+
+
+
+m.inicio();
+document.querySelector("#borrar").addEventListener("click", m.borrarCalculadora);
